372890ba0b1ff0ac33b17d93d59e49bad68b7e8a
guymolnar <guymolnar2000@gmail.com>
2021-08-27 23:09:35 +0300

    Initial Relese

---
+++ Files/dsds.js
@@ -0,0 +1,8 @@
+function SHow() {
+  var x = document.getElementById("Password");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
---
+++ Files/index.js
@@ -0,0 +1,5 @@
+
+function Confirm(){
+    y = document.getElementsByName("Pass"); 
+    alert(y);
+}
